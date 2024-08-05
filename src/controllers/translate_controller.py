5a2c99326e3289eecf8718190e8627c82102ed3c
Izadora Braga <izadora_abraga@outlook.com>
2024-08-05 19:29:34 -0300

    requisito 6

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from src.models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 translate_controller = Blueprint('translate_controller', __name__)
 
@@ -19,4 +20,27 @@ def index():
         translate_from=translate_from,
         translate_to=translate_to,
         translated=translated
+    )
+
+
+@translate_controller.route("/", methods=["POST"])
+def translate():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    translator = GoogleTranslator(source=translate_from, target=translate_to)
+
+    try:
+        translated_text = translator.translate(text_to_translate)
+    except Exception as e:
+        translated_text = f"Erro na tradução: {str(e)}"
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated_text
     )
