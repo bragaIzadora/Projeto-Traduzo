f62e33b872c9f6884af62534df24043bcb3831d6
Izadora Braga <izadora_abraga@outlook.com>
2024-08-05 17:00:13 -0300

    requisito 2 e 3

---
+++ src/models/language_model.py
@@ -0,0 +1,14 @@
+from src.models.abstract_model import AbstractModel
+from src.database.db import db
+
+class LanguageModel(AbstractModel):
+    _collection = db.languages
+
+    def __init__(self, data):
+        super().__init__(data)
+
+    def to_dict(self):
+        return {
+            "name": self.data.get("name"),
+            "acronym": self.data.get("acronym")
+        }
