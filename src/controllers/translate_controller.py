1a88d817d7c58b904efdc18b16d90e08b7009e0f
Izadora Braga <izadora_abraga@outlook.com>
2024-08-05 20:10:16 -0300

    requisito 7

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, request
 from src.models.language_model import LanguageModel
 from deep_translator import GoogleTranslator
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 translate_controller = Blueprint('translate_controller', __name__)
 
@@ -43,4 +45,27 @@ def translate():
         translate_from=translate_from,
         translate_to=translate_to,
         translated=translated_text
+    )
+
+
+@translate_controller.route("/reverse", methods=["POST"])
+def reverse_translate():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    reversed_translate_from = translate_to
+    reversed_translate_to = translate_from
+
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=translated,
+        translated=text_to_translate,
+        translate_from=reversed_translate_from,
+        translate_to=reversed_translate_to
     )
