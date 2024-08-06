853980be2c444a90694a3f4fab2baf6a9267348f
Izadora Braga <izadora_abraga@outlook.com>
2024-08-06 15:06:30 -0300

    requisito 8 em andamento

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,27 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
-def test_request_history():
-    raise NotImplementedError
+def test_request_history(prepare_base):
+    history_json = HistoryModel.list_as_json()
+    history_data = json.loads(history_json)
+
+    expected_data = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+
+    assert len(history_data) == len(expected_data)
+    for expected, actual in zip(expected_data, history_data):
+        assert expected["text_to_translate"] == actual["text_to_translate"]
+        assert expected["translate_from"] == actual["translate_from"]
+        assert expected["translate_to"] == actual["translate_to"]
